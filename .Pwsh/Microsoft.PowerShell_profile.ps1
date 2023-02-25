dfd60e1a7839a2720ab9a583665720c30a430ec5
PedroS <rochapedro2009@hotmail.com>
2023-02-24 21:56:40 -0300

    CI/CD

---
+++ .Pwsh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,28 @@
+﻿# Importando módulos
+
+Import-Module -Name posh-git -RequiredVersion 1.1.0
+Import-Module PSReadLine
+Import-Module -Name Terminal-Icons
+Import-Module $PSScriptRoot\src\main.psm1
+Import-Module $PSScriptRoot\src\libs\powerls.psm1
+Import-Module $PSScriptRoot\src\libs\PowerTouch.psm1
+
+Set-Theme otonii
+
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+# Importando configurações do PSReadLine
+Import-Module $PSScriptRoot\src\configs\PsReadLineConf.psm1
+
+# Sobrescrevendo a função ls
+New-Alias -Name ls -Value PowerLS -Option AllScope -Force
+
+# Custom Touch
+New-Alias -Name touch -Value PowerTouch -Option AllScope -Force
+# Limpando Console
+Clear-Host
+
+Invoke-Expression (&starship init powershell)
